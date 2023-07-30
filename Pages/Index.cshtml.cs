57d0bc6e87223814c6c42ca526139621370deb5e
kuldeepsingh-in <in.singhkuldeep@gmail.com>
2023-07-30 12:40:48 +0100

    Azure images rendered

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AzureIcons.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<string> Folders { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IActionResult OnGet()
         {
-            _logger = logger;
+            Folders = GetFoldersList();
+            return Page();
         }
 
-        public void OnGet()
+        public IActionResult OnGetGetFolders()
         {
+            return new JsonResult(GetFoldersList());
+        }
 
+        public IActionResult OnGetGetSvgFiles(string folder)
+        {
+            var svgFiles = GetSvgFilesForFolder(folder);
+            return new JsonResult(svgFiles);
+        }
+
+        private List<string> GetFoldersList()
+        {
+            var wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Azure_Public_Service_Icons_V15/Icons");
+            return Directory.GetDirectories(wwwrootPath)
+                            .Select(dir => Path.GetFileName(dir))
+                            .ToList();
+        }
+
+        private List<string> GetSvgFilesForFolder(string folder)
+        {
+            var wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Azure_Public_Service_Icons_V15/Icons");
+            var folderPath = Path.Combine(wwwrootPath, folder);
+            return Directory.GetFiles(folderPath, "*.svg")
+                            .Select(file => Path.GetFileName(file))
+                            .ToList();
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('/Index?handler=GetFolders') // Send a GET request to the Razor Page handler to get the folders list
+        .then(response => response.json())
+        .then(data => renderImages(data));
+});
+
+function renderImages(folders) {
+    const foldersContainer = document.getElementById('foldersContainer');
+
+    folders.forEach(folder => {
+        const folderDiv = document.createElement('div');
+        folderDiv.innerHTML = `<h2>${folder}</h2>`;
+        folderDiv.classList.add('row');
+        foldersContainer.appendChild(folderDiv);
+
+        fetch(`/Index?handler=GetSvgFiles&folder=${encodeURIComponent(folder)}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(svgFile => {
+
+                    const imageTextDiv = document.createElement('div');
+                    imageTextDiv.classList.add('col-md-2');
+                    imageTextDiv.classList.add('col-md-3');
+                    const img = document.createElement('img');
+                    const iconName = svgFile.split('.')[0]; // Remove the file extension
+                    const iconParts = iconName.split('-');
+                    const combinedName = iconParts.slice(3).join('-'); // Combine the last two parts
+
+                    img.src = `Azure_Public_Service_Icons_V15/Icons/${folder}/${svgFile}`;
+                    img.classList.add('height5');
+                    imageTextDiv.appendChild(img);
+
+                    const caption = document.createElement('p');
+                    caption.textContent = combinedName;
+                    imageTextDiv.appendChild(caption);
+
+                    folderDiv.appendChild(imageTextDiv);
+                });
+            });
+    });
+}
